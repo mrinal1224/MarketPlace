2edd40b60abb2001bf6d602f7cb65c695445b365
mrinal1224 <iammrinalkc@gmail.com>
2021-04-24 21:31:00 +0530

    router added

--- market/src/App.js
+++ market/src/App.js
@@ -3,22 +3,26 @@ import Header from "./components/Header"
 import {Container} from 'react-bootstrap'
 import Footer from "./components/Footer"
 import HomeScreen from "./screens/HomeScreen"
+import ProductScreen from "./screens/ProductScreen"
+import {BrowserRouter as Router, Route} from "react-router-dom"
 
 const App =() => {
   return (
-      <>
+      <Router>
     <Header/>
     <main className="py-3">
     <Container>
    
     
-    <HomeScreen/>
+   <Route path="/" component={HomeScreen} exact/>
+   <Route path="/product/:id" component={ProductScreen}/>
  
     </Container>
        
     </main>
     <Footer/>
-      </>
+
+      </Router>
  
    
   );
---
+++ market/src/screens/ProductScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductScreen = () => {
+    return (
+        <>
+            <h1>Product Page</h1>
+        </>
+    )
+}
+
+export default ProductScreen
