57a947181160f879650d770527f7c0f7dc3a9f50
mrinal1224 <iammrinalkc@gmail.com>
2021-04-27 01:15:06 +0530

    sensitive info removed

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const products = require('./data/products')
+const dotenv = require('dotenv')
 
-const app = express();
+dotenv.config();
 
-app.listen(5000 , console.log("Serving on Port 5000"));
+const app = express(); 
+
+const PORT= process.env.PORT || 5000
+const mode = process.env.NODE_ENV
+
+app.listen(PORT , console.log(`Server running in port ${PORT} in ${mode} mode`));
 
 app.get('/' , (req , res)=>{
  res.send("Api is Running")
