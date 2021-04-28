61a2f8ba8e995e5443bc2b582c3d629a05d24f8c
mrinal1224 <iammrinalkc@gmail.com>
2021-04-28 21:50:17 +0530

    schema models prepared

---
+++ backend/models/orderModel.js
@@ -0,0 +1,94 @@
+import mongoose from "mongoose"
+
+const orderSchema = mongoose.Schema({
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        required:true,
+        ref:"User"
+        
+    },
+    orderItems:[{
+        name:{typr:String , required:true},
+        qty:{typr:Number , required:true},
+        image:{typr:String , required:true},
+        price:{typr:Number , required:true},
+        product:{
+            type:mongoose.Schema.Types.ObjectId,
+            required:true,
+            ref:'Product'
+        
+        },
+
+    }],
+     shippingAddress:{
+     address:{type :String , required :true},
+     city:{type:String , required:true},
+     postalCode:{type:Number , required:true},
+     city:{type:String , required:true},
+     country:{type:String , required:true}
+        
+    },
+     paymentMethod:{
+        type:String,
+        required:true,
+       
+    },
+     paymentResult:{
+        id:{type:String},
+        status:{type:String},
+        updateTime:{type:String},
+        email_address:{type:String},
+        
+
+    },
+
+     taxPrice:{
+        type:Number,
+        required:true,
+        default:0.0
+    },
+     shippingPrice:{
+        type:Number,
+        required:true,
+        default:0.0
+    },
+     taxPrice:{
+        type:Number,
+        required:true,
+        default:0.0
+    },
+     totalPrice:{
+        type:Number,
+        required:true,
+        default:0.0
+    },
+    isPaid:{
+        type:Boolean,
+        required:true,
+        default:false
+    },
+    paidAt:{
+        type:Date
+    },
+    isDeivered:{
+        type:Boolean,
+        required:true,
+        default:false,
+    },
+    deliveredAt:{
+        type:Date,
+    }
+
+
+
+
+
+
+} ,
+{
+    timeStamps:true
+})
+
+const Order = mongoose.model("Order" , orderSchema);
+
+export default Order                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
