d30de956e846496fe47c5cc7febad1517c00e539
mrinal1224 <iammrinalkc@gmail.com>
2021-05-17 02:30:54 +0530

    user Authentication and autoriation implemented (Backend)

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,4 +1,5 @@
 import asyncHandler from "express-async-handler"
+import { Error } from "mongoose"
 import User from "../models/userModel.js"
 import generateToken from "../utils/tokenGenerator.js"
 
@@ -25,6 +26,26 @@ const authUser = asyncHandler(async (req , res) =>{
 
 })
 
+
+const getUserProfile= asyncHandler(async(req , res)=>{
+    const user = await User.findById(req.user._id)
+
+
+    if(user){
+        res.json({
+           _id : user._id,
+           name: user.name,
+           email : user.email,
+           isAdmin : user.isAdmin,
+           
+       })
+
+    }else{
+        res.status(401)
+        throw new Error("user not found")
+    }
+})
+
 export{ 
- authUser
+ authUser , getUserProfile
 }
